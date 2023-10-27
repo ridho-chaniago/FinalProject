2ad316cddd54808b72deacfdaa486abb27737863
Ridho-Chaniago <ridho12c.rc@gmail.com>
2023-10-27 16:58:42 +0700

    pembaruan

--- js/home.js
+++ js/home.js
@@ -18,7 +18,7 @@ function inject() {
                         <p class="text-[#7C7C7C] text-[14px]">${product.weight}</p>
                         <div class="flex justify-between items-end">
                             <span class="font-semibold">${product.price}</span>
-                            <button onclick="">
+                            <button class="btnAddToChart">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="46" height="47" viewBox="0 0 46 47" fill="none"> <rect x="0.217773" y="0.795486" width="45.67" height="45.6684" rx="17" fill="#53B175"/> <path d="M31.5527 23.6297C31.5527 24.0055 31.4086 24.3688 31.1392 24.6319C30.876 24.9013 30.5126 25.0516 30.1366 25.0516H24.472V30.7141C24.472 31.0899 24.3216 31.4532 24.0522 31.7163C23.789 31.9794 23.4318 32.1297 23.0559 32.1297C22.6799 32.1297 22.3165 31.9794 22.0533 31.7163C21.7838 31.4532 21.6335 31.0899 21.6335 30.7141V25.0516H15.9689C15.5929 25.0516 15.2295 24.9013 14.9663 24.6319C14.7031 24.3688 14.5527 24.0055 14.5527 23.6297C14.5527 23.2539 14.7031 22.8968 14.9663 22.6275C15.2295 22.3644 15.5929 22.2141 15.9689 22.2141H21.6335V16.5516C21.6335 16.1758 21.7838 15.8125 22.0533 15.5494C22.3165 15.28 22.6799 15.1297 23.0559 15.1297C23.4318 15.1297 23.789 15.28 24.0522 15.5494C24.3216 15.8125 24.472 16.1758 24.472 16.5516V22.2141H30.1366C30.5126 22.2141 30.876 22.3644 31.1392 22.6275C31.4086 22.8968 31.5527 23.2539 31.5527 23.6297Z" fill="white"/>
                             </svg>
                             </button>
@@ -30,6 +30,32 @@ function inject() {
     addProductToHome();
   });
 }
-
 inject();
-// addProductToHome();
+
+// penambahan angka di logo keranjang
+
+let countChart = 0;
+function addToChart() {
+  countChart++;
+
+  const chartLogo = document.getElementById("chartLogo");
+  const textChartLogo = document.createElement("span");
+  textChartLogo.className =
+    "absolute right-[-12px] bottom-[20px] bg-green-500 rounded-[100%] w-[20px] h-[20px] flex justify-center items-center";
+  textChartLogo.innerHTML = `${countChart}`;
+  chartLogo.appendChild(textChartLogo);
+}
+let btnAddToChart = document.getElementsByClassName("btnAddToChart");
+console.log(btnAddToChart);
+for (let i = 0; i < btnAddToChart.length; i++) {
+  btnAddToChart[i].addEventListener("click", addToChart);
+}
+
+// Menghitung jumlah angka genap dari 1 hingga 10
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    sum += i;
+    console.log(i);
+  }
+}
